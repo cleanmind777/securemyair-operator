e0629f57c5967d30ec2ced3c906c40f044f52b20
cleanmind777 <cleanmind77777@gmail.com>
2026-01-28 23:29:15 -0800

    initial project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/** @format */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost/php/operator';
+// axios.defaults.baseURL = 'https://api.securemyair.com/operator'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
